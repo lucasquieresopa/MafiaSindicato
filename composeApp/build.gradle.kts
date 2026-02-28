65a68992508543c34738ff6427c15ed1ec18e90d
lucasquieresopa <101714093+lucasquieresopa@users.noreply.github.com>
2026-02-28 18:40:13 -0300

    Added dependencies to correct serialization

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 kotlin {
